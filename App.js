ff57411782580fed34ef592895c6e6eba97e72aa
hiagopsilva <hiagohps360@gmail.com>
2020-06-20 20:39:21 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </>
+  );
+};
+
+export default App;
