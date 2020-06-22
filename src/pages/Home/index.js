8ecef44405072ab5e6d0cf54118e332eb55d3cca
hiagopsilva <hiagohps360@gmail.com>
2020-06-22 05:01:07 -0300

    refactor code

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,25 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, ImageBackground, ActivityIndicator} from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
+import {ImageBackground} from 'react-native';
 import GetLocation from 'react-native-get-location';
 import api from '../../services/api';
 
-import {
-  Prevision,
-  Header,
-  HeaderTitle,
-  MainPrevision,
-  TitleMain,
-  FooterPrevision,
-  TitlePrevision,
-  ValuePrevision,
-  LocationPrevision,
-  TitleInfo,
-  TitleLocation,
-  ButtonUpdate,
-  ContainerLoading,
-  TextLoading,
-} from './styles';
+import HeaderComponent from '../../components/Header';
+import PrevisionComponent from '../../components/Prevision';
+import Loading from '../../components/Loading';
+import ButtonComponent from '../../components/Button';
+
+import Night from '../../assets/night.jpg';
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
@@ -46,13 +35,6 @@ const Home = () => {
     getHour();
   }, []);
 
-  function getHour() {
-    const dataHours = new Date();
-    const now = dataHours.getHours();
-
-    setCurrentHour(now);
-  }
-
   async function getDataPrevison() {
     setLoading(true);
     const {latitude, longitude} = await GetLocation.getCurrentPosition({
@@ -68,84 +50,23 @@ const Home = () => {
     setLoading(false);
   }
 
+  function getHour() {
+    const dataHours = new Date();
+    const now = dataHours.getHours();
+
+    setCurrentHour(now);
+  }
+
   return (
-    <ImageBackground
-      source={require('../../assets/morning.jpg')}
-      style={{flex: 1}}>
-      <Header>
-        <HeaderTitle>
-          Challenge Builders <Icon name="sun" size={20} color="#fff" />
-        </HeaderTitle>
-      </Header>
+    <ImageBackground source={Night} style={{flex: 1}}>
+      <HeaderComponent />
 
       {loading == true ? (
-        <ContainerLoading>
-          <ActivityIndicator color="#FFF" size="large" />
-          <TextLoading>Carregando...</TextLoading>
-        </ContainerLoading>
+        <Loading />
       ) : (
-        <Prevision>
-          <MainPrevision>
-            {DataPrevisionWeather.weather[0].description == 'céu limpo' &&
-            currentHour <= 18 &&
-            currentHour >= 5 ? (
-              <Icon name="sun" size={60} color="#fff" />
-            ) : (
-              <></>
-            )}
-            {DataPrevisionWeather.weather[0].description == 'nublado' ||
-            DataPrevisionWeather.weather[0].description ==
-              'céu pouco nublado' ||
-            DataPrevisionWeather.weather[0].description ==
-              'nuvens dispersas' ? (
-              <>
-                <Icon name="cloud" size={60} color="#fff" />
-              </>
-            ) : (
-              <></>
-            )}
-            {DataPrevisionWeather.weather[0].description == 'chuva fraca' ||
-            DataPrevisionWeather.weather[0].description == 'chuva forte' ? (
-              <Icon name="cloud-rain" size={60} color="#fff" />
-            ) : (
-              <></>
-            )}
-            {(DataPrevisionWeather.weather[0].description == 'céu limpo' &&
-              currentHour >= 18) ||
-            currentHour <= 5 ? (
-              <Icon name="moon" size={60} color="#fff" />
-            ) : (
-              <></>
-            )}
-            <TitleMain>{DataPrevisionWeather.main.temp ^ 0}º</TitleMain>
-          </MainPrevision>
-
-          <FooterPrevision>
-            <View style={{alignItems: 'center'}}>
-              <ValuePrevision>
-                {DataPrevisionWeather.main.temp_min ^ 0}º
-              </ValuePrevision>
-              <TitlePrevision>min</TitlePrevision>
-            </View>
-            <View style={{alignItems: 'center'}}>
-              <ValuePrevision>
-                {DataPrevisionWeather.main.temp_max ^ 0}º
-              </ValuePrevision>
-              <TitlePrevision>max</TitlePrevision>
-            </View>
-          </FooterPrevision>
-
-          <LocationPrevision>
-            <TitleInfo>{DataPrevisionWeather.weather[0].description}</TitleInfo>
-            <TitleLocation>
-              {DataPrevisionWeather.name}, {DataPrevisionWeather.sys.country}
-            </TitleLocation>
-          </LocationPrevision>
-        </Prevision>
+        <PrevisionComponent DataPrevision={DataPrevisionWeather} />
       )}
-      <ButtonUpdate onPress={() => getDataPrevison()}>
-        <Text>Atualizar</Text>
-      </ButtonUpdate>
+      <ButtonComponent getDataPrevision={() => getDataPrevison()} />
     </ImageBackground>
   );
 };
