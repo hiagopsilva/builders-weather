3de6a23edf0dcec9d1a9406e7bf3782cb6619c9b
hiago <hiagohps360@gmail.com>
2021-04-28 23:28:22 -0300

    feat: api request functions

---
+++ src/api/weather.api.ts
@@ -0,0 +1,18 @@
+import {request} from '~/services';
+
+class WeatherAPI {
+  static getDataWeather = async (
+    latitude?: number,
+    longitude?: number,
+  ): Promise<any | null> => {
+    try {
+      const {data} = await request.get(
+        `weather?lat=${latitude?.toString()}&lon=${longitude?.toString()}&&units=metric&lang=pt&APPID=13fe761107ed465fe627448321516f2f`,
+      );
+      return data || null;
+    } catch (error) {
+      return null;
+    }
+  };
+}
+export default WeatherAPI;
