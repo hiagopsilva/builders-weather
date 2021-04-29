e86937370dd9b1668892db727f70b94f138feb9a
hiago <hiagohps360@gmail.com>
2021-04-29 04:07:49 -0300

    feat: colors theme

---
+++ src/theme/colors.ts
@@ -0,0 +1,44 @@
+export default {
+  failure: '',
+  warning: '',
+  success: '',
+  info: '',
+  disabled: '',
+
+  primary: {
+    light: '',
+    main: '',
+    dark: '',
+    contrast: '',
+  },
+  secondary: {
+    light: '',
+    main: '',
+    dark: '',
+    contrast: '',
+  },
+  tertiary: {
+    light: '',
+    main: '',
+    dark: '',
+    contrast: '',
+  },
+  accent: {
+    light: '',
+    main: '',
+    dark: '',
+    contrast: '',
+  },
+  fifth: {
+    light: '',
+    main: '',
+    dark: '',
+    contrast: '',
+  },
+  opacity: {
+    light: '',
+    main: '',
+    dark: '',
+    contrast: '',
+  },
+};
