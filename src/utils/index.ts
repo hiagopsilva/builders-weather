1b88c1fd48eaca99685999d1a6e88f7cddbadf42
hiago <hiagohps360@gmail.com>
2021-04-29 03:53:29 -0300

    feat: helper platform builders

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,3 @@
 // export * from './helpers';
-// export * from './modules';
+export * from './modules';
 export * from './types';
---
+++ src/utils/modules/index.ts
@@ -0,0 +1,5 @@
+export {
+  moderateScale as moderateScaleModules,
+  verticalScale as verticalScaleModules,
+} from '@platformbuilders/helpers/native';
+export { getTheme } from '@platformbuilders/helpers';
