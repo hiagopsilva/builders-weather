9ba2cf7176108c2cbf76996412385970c3269c72
hiago <hiagohps360@gmail.com>
2021-04-28 23:30:04 -0300

    refactor: prevision component

---
+++ src/components/Prevision/index.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable no-bitwise */
+import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {
+  PrevisionContainer,
+  MainPrevision,
+  TitleMain,
+  WrapperPrevision,
+  Title,
+  Value,
+  WrapperLocation,
+  TitleInfo,
+  TitleLocation,
+  WrapperDataPrevision,
+} from './styles';
+
+const Prevision = (dataPrevision: ValuesPrevision) => {
+  const {DataPrevision} = dataPrevision;
+
+  console.log(DataPrevision);
+
+  return (
+    <PrevisionContainer>
+      <MainPrevision>
+        <Icon name="cloud" size={60} color="#fff" />
+        <TitleMain>{DataPrevision.main.temp ^ 0}º</TitleMain>
+      </MainPrevision>
+
+      <WrapperDataPrevision>
+        <WrapperPrevision>
+          <Value>{DataPrevision.main.temp_min ^ 0}º</Value>
+          <Title>min</Title>
+        </WrapperPrevision>
+
+        <WrapperPrevision>
+          <Value>{DataPrevision.main.temp_max ^ 0}º</Value>
+          <Title>max</Title>
+        </WrapperPrevision>
+      </WrapperDataPrevision>
+
+      <WrapperLocation>
+        <TitleInfo>{DataPrevision.weather[0].description}</TitleInfo>
+        <TitleLocation>
+          {DataPrevision.name}, {DataPrevision.sys.country}
+        </TitleLocation>
+      </WrapperLocation>
+    </PrevisionContainer>
+  );
+};
+
+export default Prevision;
---
+++ src/components/Prevision/styles.ts
@@ -0,0 +1,50 @@
+import styled from 'styled-components/native';
+
+export const PrevisionContainer = styled.View`
+  flex: 1;
+  justify-content: space-between;
+`;
+
+export const MainPrevision = styled.View`
+  align-items: center;
+  margin-top: 40px;
+`;
+
+export const TitleMain = styled.Text`
+  font-size: 60px;
+  color: #fff;
+`;
+
+export const WrapperDataPrevision = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-size: 16px;
+`;
+
+export const Value = styled.Text`
+  color: #fff;
+  font-size: 40px;
+`;
+
+export const WrapperLocation = styled.View`
+  padding: 0 15px 70px;
+`;
+
+export const TitleInfo = styled.Text`
+  color: #fff;
+  font-size: 30px;
+  text-transform: capitalize;
+`;
+
+export const TitleLocation = styled.Text`
+  color: #fff;
+  font-size: 20px;
+`;
+
+export const WrapperPrevision = styled.View`
+  align-items: center;
+`;
