15eb34332e63b61b4f91e513aad9f7917a06ba32
hiago <hiagohps360@gmail.com>
2021-04-28 23:29:17 -0300

    refactor: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Icon from 'react-native-vector-icons/Feather';
+
+import {HeaderContainer, HeaderTitle} from './styles';
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <HeaderTitle>
+        Weather <Icon name="sun" size={20} color="#FFF" />
+      </HeaderTitle>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const HeaderContainer = styled.View`
+  align-items: center;
+  padding-top: 10px;
+`;
+
+export const HeaderTitle = styled.Text`
+  color: #fff;
+  font-size: 20px;
+`;
