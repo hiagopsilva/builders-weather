b73373cc33a1b98e81251f51a7c7cc96fc4455ef
hiago <hiagohps360@gmail.com>
2021-04-28 23:31:00 -0300

    chore: updated import

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+import {Home} from './scenes';
+
+const Routes = createAppContainer(createSwitchNavigator({Home}));
+
+export default Routes;
