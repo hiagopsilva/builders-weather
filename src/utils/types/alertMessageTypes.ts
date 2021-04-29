72701d0c1aed342e7a9839c41d721ed1fb6af1c7
hiago <hiagohps360@gmail.com>
2021-04-28 23:33:03 -0300

    feat: types utils

---
+++ src/utils/types/alertMessageTypes.ts
@@ -0,0 +1,17 @@
+export type PositionAlertMessage = 'top' | 'bottom' | 'center';
+
+export type MessageTypeIcon =
+  | 'none'
+  | 'default'
+  | 'info'
+  | 'success'
+  | 'danger'
+  | 'warning';
+
+export type MessageType =
+  | 'none'
+  | 'default'
+  | 'info'
+  | 'success'
+  | 'danger'
+  | 'warning';
